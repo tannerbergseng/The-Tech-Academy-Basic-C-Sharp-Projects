c975e6ea2c97ed443291fc0e31dfcf6fe782b3c4
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-16 15:05:18 -0700

    Add files via upload

---
+++ TTA_Drill4/TTA_Drill4/Drill4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("what is your age?");
+            string Age = Console.ReadLine();
+            int userAge = Convert.ToInt32(Age);
+            bool checkAge = Convert.ToBoolean(userAge);
+
+            Console.WriteLine("have you ever had a dui, please type 'true' or 'false'");
+            string DUICheck = Console.ReadLine();
+            bool DUIReq = Convert.ToBoolean(DUICheck);
+
+            Console.WriteLine("How many speeding tickets do you have?(Please use numerical value 1,2,ect..)");
+            string Speeding = Console.ReadLine();
+            int speedRacer = Convert.ToInt32(Speeding);
+            bool SpeedCheck = Convert.ToBoolean(speedRacer);
+
+            if (speedRacer <= 3 && userAge >= 15 && DUIReq == false)
+            {
+                    Console.WriteLine("Congrats you qualify!");
+                    Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("sorry you dont meet our requirements..");
+                Console.ReadLine();
+            }
+
+            
+   
+
+
+
+
+    }
+    }
+}
---
+++ TTA_Drill4/TTA_Drill4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int userAge = Convert.ToInt32(age);
+            bool checkAge = Convert.ToBoolean(userAge);
+
+            Console.WriteLine("Have you ever had a DUI, please type 'true' or 'false'");
+            string DUICheck = Console.ReadLine();
+            bool DUIReq = Convert.ToBoolean(DUICheck);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string Speeding = Console.ReadLine();
+            int speedRacer = Convert.ToInt32(Speeding);
+            bool speedRacerCheck = Convert.ToBoolean(speedRacer);
+
+            bool qualified = true && true && true;
+
+            if (qualified)
+            {
+                bool = checkAge && DUIReq && speedRacerCheck;
+            }
+
+        }
+    }
+}
