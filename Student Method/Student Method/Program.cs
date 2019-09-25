90ec992ac9d31405566bd7459103deffb0b086bd
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-25 15:01:49 -0700

    Add files via upload

---
+++ Student Method/Student Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("See if you made the honor roll, enter your First name, your major, and your GPA");
+            string name = Console.ReadLine();
+            string major = Console.ReadLine();
+            double GPA = Convert.ToDouble(Console.ReadLine());
+            Student student1 = new Student(name, major, GPA);
+            
+
+            Console.WriteLine(student1.hasHonors());
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student Method/Student Method/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill11
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+
+        public bool hasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
