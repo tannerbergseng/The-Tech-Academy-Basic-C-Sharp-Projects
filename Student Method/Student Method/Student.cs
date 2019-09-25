eca0cecad821b3e2cebda95ff8429838b9e9b5a6
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-25 15:02:37 -0700

    Add files via upload

---
+++ Student Method/Student Method/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill11
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+
+        public bool hasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                Console.WriteLine("Congrats you made it on the honor roll!");
+                return true;
+            }
+            Console.WriteLine("Sorry you didn't quite make the honor roll");
+            return false;
+        }
+    }
+}
