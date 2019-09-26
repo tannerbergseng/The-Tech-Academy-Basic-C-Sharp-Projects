25d458ab18bd8de9349c04ef2b56dc7dcc7efb27
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-26 14:18:50 -0700

    Add files via upload

---
+++ TTA_Drill12/TTA_Drill12/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill12
+{
+    class Employee
+    {
+        public double payRate;
+        public double hoursWorked;
+
+        static void Employee1(double payRate, double hoursWorked)
+        {
+            double weeklyEarning = payRate * hoursWorked;
+        }
+    }
+}
---
+++ TTA_Drill12/TTA_Drill12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your pay rate and your hours worked and I will determine how much you can expect to make in a week.");
+            double payRate = Convert.ToDouble(Console.ReadLine());
+            double hoursWorked = Convert.ToDouble(Console.ReadLine());
+            
+            Employee employee = new Employee (payRate, hoursWorked);
+            
+            Console.ReadLine();
+        }
+    }
+}
