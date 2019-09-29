42fb0a78af446fff801d6cc51a3fa01a15a39c1d
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-28 17:28:29 -0700

    Add files via upload

---
+++ TTA_Drill13/TTA_Drill13/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill12
+{
+    public class Employee
+    {
+        public void employeeMethod(double payRate, double hoursWorked)
+        {
+            double weeklyEarning = payRate * hoursWorked;
+            Console.WriteLine("You worked: " + hoursWorked + " hours, and you made: $" + weeklyEarning + " this week");
+        }
+    }
+}
