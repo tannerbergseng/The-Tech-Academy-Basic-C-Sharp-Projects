9065dcee321417ba17b9ccdfcdc539bbae82966b
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-09-30 16:25:38 -0700

    Add files via upload

---
+++ TTA_Drill14/TTA_Drill14/ExampleClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace TTA_Drill14
+{
+    class ExampleClass
+    {
+        
+        public void ExampleMethod(int required,  int optionalInt = 10)
+        {
+            int result = required + optionalInt;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TTA_Drill14/TTA_Drill14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace TTA_Drill14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleClass anExample = new ExampleClass();
+            Console.WriteLine("Type in an integer");
+            int required = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now type in another or leave it blank");
+            string optionalInt = Console.ReadLine();
+            if (optionalInt == "")
+            {
+                anExample.ExampleMethod(required);
+            }
+            else
+            {
+                int int2 = Convert.ToInt32(optionalInt);
+                anExample.ExampleMethod(required, int2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
