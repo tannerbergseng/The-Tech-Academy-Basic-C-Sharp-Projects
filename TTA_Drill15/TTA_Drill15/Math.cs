7bbcb26b714491218afcda5b30879d12517ff96a
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-03 13:55:23 -0700

    Add files via upload

---
+++ TTA_Drill15/TTA_Drill15/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill15
+{
+    class Math
+    {
+        public void mathMethod(int a, int b = 2)
+        {
+            int result = a / b;
+            Console.WriteLine(result);
+        }
+        public void mathMethod(int a , int b, int c)
+        {
+            int sum = a + b + c;
+            Console.WriteLine(sum);
+        }
+    }
+}
