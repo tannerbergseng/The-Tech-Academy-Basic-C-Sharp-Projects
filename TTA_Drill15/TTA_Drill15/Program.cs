19ce024b574ab432eeed7002efd3781a8b3092ff
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-03 14:56:30 -0700

    Add files via upload

---
+++ TTA_Drill15/TTA_Drill15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill15
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math aMethod = new Math();
+            Console.WriteLine("Type in an integer and it will be divided by 2");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            aMethod.mathMethod(a);
+            Console.ReadLine();
+            
+            Console.WriteLine("Now add two more integers");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            aMethod.mathMethod(a,b,c);
+            Console.ReadLine();
+
+            aMethod.Addition(out int i);
+            Console.ReadLine();
+        }
+       
+    }
+}
