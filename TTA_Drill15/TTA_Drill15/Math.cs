a3273b8c3bc6d99ac390f111e8c7fe97147cd27c
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-04 16:26:30 -0700

    Add files via upload

---
+++ TTA_Drill15/TTA_Drill15/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill15
+{
+     static class Math
+    {
+        public static void MathMethod(int a, out int result)
+        {
+            result = a / 2;
+        }
+        public static void MathMethod(int a, int b, int c, out int sum)
+        {
+            sum = a + b + c;
+        }
+
+        public static void Addition(out int i)
+        {
+            i = 30;
+            i += i;
+            Console.WriteLine("Here is the output method: " + i);
+
+        }
+    }
+}
---
+++ TTA_Drill15/TTA_Drill15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math aMethod = new Math();
+            Console.WriteLine("Type in an integer and it will be divided by 2");
+            int t = Convert.ToInt32(Console.ReadLine());
+            
+            Math.MathMethod(t, out int x);
+            Console.WriteLine("Your number divided by 2: " + x);
+            Console.ReadLine();
+            
+            Console.WriteLine("Now add two more integers");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Math.MathMethod(t,b,c, out int sum);
+            Console.WriteLine("The sum of all numbers is "+ sum);
+            Console.ReadLine();
+
+            Math.Addition(out int i);
+            Console.ReadLine();
+        }
+       
+    }
+}
