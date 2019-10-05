da551a7c3faaacf970431e24985bd431c9eaedb4
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-04 17:49:31 -0700

    Add files via upload

---
+++ TTA_Drill16/TTA_Drill16/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill16
+{
+    public class Person
+    {
+        public List<string> Employee { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+
+        public void sayName()
+        {
+            Console.WriteLine("Name: {0} {1}", fName, lName);
+        }
+    }
+}
---
+++ TTA_Drill16/TTA_Drill16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.fName = "Sample";
+            employee.lName = "Student";
+            employee.sayName();
+            Console.ReadLine();
+        }
+    }
+}
