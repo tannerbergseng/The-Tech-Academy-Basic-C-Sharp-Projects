70722b0a2aca04a9917859200c5605ae825bf656
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-07 14:55:59 -0700

    Add files via upload

---
+++ TTA_Drill17/TTA_Drill17/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill17
+{
+   class Employee : Person
+    {
+        public override void sayName()
+        {
+            Console.WriteLine(fName);
+            Console.WriteLine(lName);
+        }
+    }
+}
---
+++ TTA_Drill17/TTA_Drill17/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill17
+{
+    abstract class Person
+    {
+        public string fName;
+        public string lName;
+
+        public abstract void sayName();
+    }
+}
