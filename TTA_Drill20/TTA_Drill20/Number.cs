62610263fb4dfba66d7c07d6a05488d16f1975a0
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-09 16:13:55 -0700

    Add files via upload

---
+++ TTA_Drill20/TTA_Drill20/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill20
+{
+    struct Number
+    {
+       public decimal Amount;
+    }
+}
---
+++ TTA_Drill20/TTA_Drill20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amnt;
+            amnt.Amount = 1.123m;
+
+            Console.WriteLine(amnt.Amount);
+            Console.ReadLine();
+        }
+    }
+}
