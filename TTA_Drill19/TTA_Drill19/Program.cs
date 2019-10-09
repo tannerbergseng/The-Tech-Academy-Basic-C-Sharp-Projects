6791cbeaef4127e2199b00ce1a3dcee28f18db60
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-09 14:52:05 -0700

    Add files via upload

---
+++ TTA_Drill19/TTA_Drill19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week");
+            string userInput = Console.ReadLine();
+           
+            try
+            {
+                daysOfTheWeek day = (daysOfTheWeek) Enum.Parse(typeof(daysOfTheWeek), userInput, true);
+                Console.WriteLine("you entered: " + day);
+                Console.ReadLine();
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+        public enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
