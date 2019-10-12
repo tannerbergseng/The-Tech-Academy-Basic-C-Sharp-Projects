3d5ec9d7253536dd175ddd58ddaf797256ce64eb
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-11 18:49:15 -0700

    Add files via upload

---
+++ TTA_Drill23/TTA_Drill23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            DateTime CurrentTime = DateTime.Now;
+            Console.WriteLine("type in a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DateTime ChangedHour = CurrentTime.AddHours(number);
+            Console.WriteLine("It will be {0} in {1} hour(s) ", ChangedHour, number);
+            Console.ReadLine();
+        }
+
+    }
+}
