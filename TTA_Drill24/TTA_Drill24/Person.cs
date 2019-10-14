cc0cc3cb41979ce7f9d941e9cc32bf91770244fe
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-14 14:27:13 -0700

    Add files via upload

---
+++ TTA_Drill24/TTA_Drill24/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill24
+{
+    class Person
+    {
+        public Person(string name): this(name, 0)
+        {
+        }
+        public Person(string name, int age)
+        {
+           
+        }
+    }
+}
