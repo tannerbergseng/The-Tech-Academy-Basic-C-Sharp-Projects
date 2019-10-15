3f2887f250c4e459fd1d3e2e29d399a6a255f11c
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-10-14 17:13:01 -0700

    Add files via upload

---
+++ TTA_Drill24/TTA_Drill24/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill24
+{
+    abstract class Person
+    {
+        public int YourAge = Convert.ToInt32(Console.ReadLine());
+             
+        
+        public abstract void sayAge();
+
+    }
+}
---
+++ TTA_Drill24/TTA_Drill24/PersonAge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTA_Drill24
+{
+    class PersonAge : Person
+    {
+        public override void sayAge()
+        {
+            if (YourAge <= 0)
+            {
+                throw new AgeException();
+            }
+            else
+            {
+                Console.WriteLine("You entered {0} as your age", YourAge);
+            }
+        }
+        
+
+    }
+}
+
